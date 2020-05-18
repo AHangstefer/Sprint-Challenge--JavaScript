456cdc01edc3633dc9628dc64a4cfce951029c6c
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-05-18 02:07:58 -0400

    complete except questions in read me

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -53,15 +53,19 @@ The zoos are concerned about animals with a lower population count. Using filter
   with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
-console.log(lowPopulationAnimals);
+let lowPopulationAnimals = zooAnimals.filter(animal => animal.population <=5);
+console.log (lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+The zoos need to know their total animal population across the United States.
+Find the total population from all the zoos using the .reduce() method.
+Remember the reduce method takes two arguments: a callback 
+(which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce((animals, index) =>
+animals + index.population, 0);
 console.log(populationTotal);
 
 
@@ -73,19 +77,33 @@ console.log(populationTotal);
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume(a, b, cb){
+  return cb(a,b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
+  * Create a function named greeting that accepts a first and last name and 
+  * returns "Hello first-name last-name, nice to meet you!"
+*/ 
+function add(num, num){
+  return num +num;
+}
+
+function multiply(num1, num){
+  return num1*num;
+}
+
+function greeting (first, last){
+  return `Hello ${first} ${last}, nice to meet you!`
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+ console.log(consume(2, 2, add)); // 4
+ console.log(consume(10, 16, multiply)); // 160
+ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
