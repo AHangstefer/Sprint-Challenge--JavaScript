f824091cc30cdb2c1924944fdff4b946c38d20bb
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-05-17 22:22:41 -0400

    jk..finished classes

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -12,7 +12,7 @@ class CuboidMaker{
   constructor(length, width, height){
     this.length = length;
     this.width = width;
-    this.height = height;
+    this.height = height
   }
 }
 const cubie = new CuboidMaker(2,3,4);
@@ -44,7 +44,7 @@ surfaceArea(){
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
-const cuboid = new CuboidMaker(4,5,6);
+const cuboid = new CuboidMaker(4,5,5);
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
  console.log(cuboid.volume()); // 100
